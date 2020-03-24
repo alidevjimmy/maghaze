3c39845ee4c48b152bc354c2e383c6bcf2f610f1
alidevjimmy <59659737+alidevjimmy@users.noreply.github.com>
2020-03-24 18:39:19 +0430

    hangout...

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -4,36 +4,42 @@ namespace App\GraphQL\Mutations;
 
 use App\User;
 use Illuminate\Support\Arr;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+
 
 class AuthMutator
 {
-    public function login($root , array $args)
+    public function login($root, array $args)
     {
-        $credentials = Arr::only($args , ['phone' , 'password']);
-        $credentials = ['phone' => $credentials['phone'] , 'password' => $credentials['password']];
+        $credentials = Arr::only($args, ['phone', 'password']);
         try {
-            if (!$token = auth()->attempt($credentials)) {
-                return ['token' => null];
+            if ($token =  JWTAuth::attempt($credentials)) {
+                return $token;
             }
         } catch (JWTException $e) {
-            return ['token' => null];
+            return null;
         }
-        return [
-            'token' => compact('token')
-        ];
+        return null;
     }
 
-    public function register($root , array $args)
+    public function register($root, array $args)
     {
         $user = User::create([
-            'phone'    => $args['phone'],
-            'name'    => $args['name'],
-            'family'    => $args['family'],
-            'password'    => $args['password'],
+            'phone' => $args['phone'],
+            'name' => $args['name'],
+            'family' => $args['family'],
+            'password' => $args['password'],
         ]);
+        $user = User::find($user->id);
         $token = auth()->login($user);
+        $user['token'] = $token;
         return $user;
+    }
 
+    public function logout()
+    {
+        auth()->logout('api');
+        return "شما خارج شدید";
     }
 }
